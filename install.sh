9059aba1e616c16fc0914c4f753f6286a1545617
Wenisaputri <96043348+Wenisaputri@users.noreply.github.com>
2021-12-20 23:00:43 +0700

    Create install.sh

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ wget -O renew-ss "https://raw.githubusercontent.com/Afdhan/esesha/main/renew-ss.
 wget -O add-host "https://raw.githubusercontent.com/Afdhan/esesha/main/add-host.sh"
 wget -O about "https://raw.githubusercontent.com/Afdhan/esesha/main/about.sh"
 wget -O menu "https://raw.githubusercontent.com/Afdhan/esesha/main/menu.sh"
-wget -O usernew "https://raw.githubusercontent.com/Afdhan/esesha/main/usernew.sh"
+wget -O usernew "https://raw.githubusercontent.com/Wenisaputri/esesha/main/usernew.sh"
 wget -O trial "https://raw.githubusercontent.com/Afdhan/esesha/main/trial.sh"
 wget -O hapus "https://raw.githubusercontent.com/Afdhan/esesha/main/hapus.sh"
 wget -O member "https://raw.githubusercontent.com/Afdhan/esesha/main/member.sh"
